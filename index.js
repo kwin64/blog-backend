ba7eaf46ba1dac051634fbe04636bccbaa4adbfd
kwin64 <kwin11bvb@yandex.by>
2022-12-25 23:38:45 +0300

    cors fix native

--- index.js
+++ index.js
@@ -28,12 +28,21 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage })
 app.use(express.json())
-app.use(
-	cors({
-		credentials: true,
-		origin: /\.herokuapp\.com$/
-	})
-)
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+// app.use(
+// 	cors({
+// 		credentials: true,
+// 		origin: /\.herokuapp\.com$/
+// 	})
+// )
 app.use('/uploads', express.static('uploads'))
 
 //Routes
