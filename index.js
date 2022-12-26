518ff942bdce703faf0a227a41e77ea642ff2e7a
kwin64 <kwin11bvb@yandex.by>
2022-12-27 00:17:43 +0300

    fix url

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json())
 app.use(
 	cors({
 		credentials: true,
-		origin: ['https://blog-beryl-phi.vercel.app/']
+		origin: ['https://blog-p8lz8hgnw-kwin64.vercel.app/']
 	})
 )
 
