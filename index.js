c8b2945c7361dceea6dcb7f7a365847d8329d614
kwin64 <kwin11bvb@yandex.by>
2022-12-26 16:39:34 +0300

    vercel config

--- index.js
+++ index.js
@@ -46,12 +46,12 @@ app.use(
 app.use('/uploads', express.static('uploads'))
 
 //Routes
-app.use('auth', authRoute)
-app.use('posts', postsRoute)
-app.post('upload', upload.single('image'), (req, res) => {
+app.use('/auth', authRoute)
+app.use('/posts', postsRoute)
+app.post('/upload', upload.single('image'), (req, res) => {
 	res.json({
 		url: `/uploads/${req.file.originalname}`
 	})
 })
 
-app.listen(process.env.PORT, () => console.log(`server started on ${process.env.PORT}`))
+app.listen(process.env.PORT || 5080, () => console.log(`server started on ${process.env.PORT}`))
